ba43399e8133fe289c77e48f1e31a72824debd16
retronbv <49005044+retronbv@users.noreply.github.com>
2024-08-28 15:52:18 -0400

    fix index & fix /balance user

--- commands/balance.js
+++ commands/balance.js
@@ -1,17 +1,33 @@
 const { SlashCommandBuilder } = require("discord.js");
+const profileModel = require("../models/profileSchema");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("balance")
-        .setDescription("check your balance")
-        .addUserOption((option) =>
-            option
-                .setName("user")
-                .setDescription("the user you want to donate to")
-        ),
-    async execute(interaction, profileData) {
-        const { balance } = profileData;
-        const username = interaction.user.username;
-        await interaction.reply(`${username} has ${balance} 🪙`)
-    },
+  data: new SlashCommandBuilder()
+    .setName("balance")
+    .setDescription("check your balance")
+    .addUserOption((option) =>
+      option.setName("user").setDescription("the user you want to donate to")
+    ),
+  async execute(interaction, profileData) {
+    let user = interaction.options.getUser("user");
+    if (!user) user = interaction.user;
+    let newProfileData;
+    try {
+      newProfileData = await profileModel.findOne({
+        userID: user.id,
+      });
+      if (!newProfileData) {
+        newProfileData = await profileModel.create({
+          userID: user.id,
+          serverID: interaction.guild.id,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    const { balance } = newProfileData;
+    const username = user.username;
+    await interaction.reply(`${username} has ${balance} 🪙`);
+  },
 };
--- index.js
+++ index.js
@@ -11,29 +11,29 @@ const { error } = require("node:console");
 
 // create a new client instance
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessageReactions,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
 });
 
 //load the events file on startup
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
-    .readdirSync(eventsPath)
-    .filter((file) => file.endsWith(".js"));
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 //load the command file on startup
@@ -41,28 +41,31 @@ for (const file of eventFiles) {
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const commands = require(filePath);
-    if ("data" in commands && "execute" in commands) {
-        client.commands.set(commands.data.name, commands);
-    } else {
-        console.log(
-            `[WARNING] The command at ${filePath} os ,ossomg a reqiored "data" or "execute" prperty`
-        );
-    }
+  const filePath = path.join(commandsPath, file);
+  const commands = require(filePath);
+  if ("data" in commands && "execute" in commands) {
+    client.commands.set(commands.data.name, commands);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} os ,ossomg a reqiored "data" or "execute" prperty`
+    );
+  }
 }
-//connect the database to the bot 
-mongoose.connect(database, {
+//connect the database to the bot
+mongoose
+  .connect(database, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     console.log("Connected to the database");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(error);
-});
+  });
 
 client.login(token);
